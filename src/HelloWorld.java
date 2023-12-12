0ad5ae348f85120650d38ada7bb9fc258591a1f1
Michael Ventura <m.ventura14@yahoo.com>
2023-12-12 11:57:00 -0600

    Starting Java

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    /*psvm or main => tab*/
+    public static void main(String[] args) {
+        /*Console.log equivalent*/
+        /*Strings must be in double quotes ""*/
+        System.out.println("Hello World");
+    }
+}
